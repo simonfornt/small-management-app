4a21e899274e93376a76ad787788e071082241db
simonfotnt <simonpiar98@gamil.com>
2025-02-26 01:37:04 +0600

    added localstorage and display current balance

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ const withdraw = document.querySelector('#withdraw');
 const transList = document.querySelector('#transList');
 const transHistroy = document.querySelector('#transHistory');
 
+
+//  use localstorage to sotre value even after reload page
+let currentBlance = parseFloat(localStorage.getItem('blance')) || 0;
+let transactionHistroy = JSON.parse(localStorage.getItem('transactionHistroy')) || [];
+
+// display the current balance
+
+balance.textContent =  `Tk: ${currentBlance.toFixed(2)}`;
