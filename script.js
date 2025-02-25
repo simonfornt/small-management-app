d19fcf9d0401adac83e167da889c8985a4924f87
simonfotnt <simonpiar98@gamil.com>
2025-02-26 01:23:17 +0600

    variables declears

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// variables declears
+
+const balance = document.querySelector('#blance');
+const amountInput = document.querySelector('#amountInput');
+const addMoney = document.querySelector('#addMoney');
+const withdraw = document.querySelector('#withdraw');
+const transList = document.querySelector('#transList');
+const transHistroy = document.querySelector('#transHistory');
+
